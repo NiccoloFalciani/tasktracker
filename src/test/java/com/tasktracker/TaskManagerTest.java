2fa3b39bd401e35d361ae392362e2eb8e376f16a
Niccolò Falciani <niccolo.falciani@merlin-zwo.de>
2026-05-28 15:38:50 +0200

    JUnit-Tests Happy Path

--- src/test/java/com/tasktracker/TaskManagerTest.java
+++ src/test/java/com/tasktracker/TaskManagerTest.java
@@ -6,13 +6,59 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TaskManagerTest {
 	
 	@Test
-	void testAddTask() {
+	void shouldAddTasktoList() {
 		TaskManager tm = new TaskManager();
 		
 		tm.createTask("Java lernen");
 		
 		assertEquals(1, tm.getTasks().size());
 		
+	}
+	
+	@Test 
+	void shouldStoreCorrectTaskTitle () {
+		TaskManager tm = new TaskManager();
+		String expectedTitle = "GIT lernen";
+		
+		tm.createTask(expectedTitle);
+		String actualTitle = tm.getTasks().get(0).getTitle();
+		
+		assertEquals(expectedTitle, actualTitle);
+		
+	}
+	
+	@Test 
+	void shouldAddMultipleTasks() {
+		TaskManager tm = new TaskManager();
+		
+		tm.createTask("Java lernen");
+		tm.createTask("JavaScript lernen");
+		tm.createTask("GIT lernen");
+		
+		assertEquals(3, tm.getTasks().size());
+
+	}
+	
+	@Test
+	void shouldPreserveTaskOrder() {
+		TaskManager tm = new TaskManager();
+		
+		tm.createTask("Java lernen");
+		tm.createTask("JavaScript lernen");
+
+		assertEquals("Java lernen", tm.getTasks().get(0).getTitle());
+		assertEquals("JavaScript lernen", tm.getTasks().get(1).getTitle());
+
+	}
+	
+	@Test
+	void shouldMarkTaskAsCompleted() {
+		TaskManager tm = new TaskManager();
+
+		tm.createTask("Java lernen");
+		tm.markTaskCompleted(0);
+		
+		assertEquals(true, tm.getTasks().get(0).isCompleted());
 	}
 
 }
